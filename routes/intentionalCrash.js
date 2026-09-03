4c9f68c79c0786a2f319d751ca1d746fb9dc395e
warren-west <teacher.warren.english@gmail.com>
2026-09-03 11:17:21 +0200

    workflows: Set a Docker workflow.

--- routes/intentionalCrash.js
+++ routes/intentionalCrash.js
@@ -3,8 +3,14 @@ const router = require('express').Router()
 // a route that crashes the instance
 router.get('/', () => {
     throw new Error('App intentionally crashed. 💀')
-    return
 })
 
+router.get('/exit', () => {
+    process.exit(1)
+})
+
+router.get('/name', () => {
+    firstName = "warren"
+})
 
 module.exports = router
